648c319be58e995fcd4d8e14038b267df37eb7c5
yasin.gunes <yasin01ysn@gmail.com>
2023-02-18 01:14:43 +0300

    Add port value

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ app.use("/", function(req, res) {
     res.send("Home");
 });
 
-app.listen(3000, () => {
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
     console.log("Listening on port 3000");
 });
